533be5c34485678c2d645336a8962b435ea8bbec
andreas.hausberger <andreas.hausberger@hpc.at>
2020-05-08 16:01:40 +0200

    [US #31] Saving & Frontend validation of Location to Event.

--- frontend/src/app/components/event-location/event-location.component.ts
+++ frontend/src/app/components/event-location/event-location.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {EventService} from "../../services/event.service";
 import {Location} from "../../dtos/location";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-event-location',
@@ -9,16 +10,25 @@ import {Location} from "../../dtos/location";
 })
 export class EventLocationComponent implements OnInit {
 
-  constructor(private eventService: EventService) { }
+  @Output() locationSaved = new EventEmitter<Location>();
+
+  constructor(private eventService: EventService,
+              private sanitizer: DomSanitizer) { }
 
   private searchComplete = false
   private addressSelected = false;
-  public searchResults: Array<any>
+  private selectionComplete = false;
+  public searchResults: Array<any>;
+  public selectedLocation: Location;
+  public previewURL: SafeResourceUrl;
+
 
   ngOnInit(): void {
+
   }
 
   performSearch() {
+    this.selectionComplete = false;
     let element: any = document.getElementById('addressSearch');
     let value = element.value;
     this.eventService.searchLocationInAPI(value).subscribe( results => {
@@ -34,7 +44,27 @@ export class EventLocationComponent implements OnInit {
     let specificName = result.address.address29 ? result.address29 : result.address.road
     let locationAddress = result.address.road + " " +  result.address.postcode + " " +  result.address.city;
      let location: Location = new Location(null, specificName, locationAddress, result.address.postcode, result.lat, result.lon);
+     this.selectedLocation = location;
      console.log(location);
+     this.createPreviewURL();
+     this.addressSelected = true;
   }
+
+  saveButtonClicked() {
+    this.searchResults = null;
+    this.selectionComplete = true;
+    this.locationSaved.emit(this.selectedLocation);
+  }
+
+  private createPreviewURL() {
+  let upperBBoxLat: number = this.selectedLocation.latitude - (-0.0002);
+  let lowerBBoxLat: number = this.selectedLocation.latitude - 0.0002;
+  let upperBBoxLon: number = this.selectedLocation.longitude - (-0.0002);
+  let lowerBBoxLon: number = this.selectedLocation.longitude - 0.0002;
+  let previewString: string = "https://www.openstreetmap.org/export/embed.html?bbox=" + lowerBBoxLon + "%2C" + lowerBBoxLat + "%2C" + upperBBoxLon + "%2C" + upperBBoxLat + "&amp;layer=mapnik&amp;marker=" + this.selectedLocation.longitude + "%2C" + this.selectedLocation.latitude;
+
+  console.log(previewString);
+  this.previewURL = this.sanitizer.bypassSecurityTrustResourceUrl(previewString)
+}
 
 }
