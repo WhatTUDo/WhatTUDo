119e7feea2bf1ae24caaa1e64e3adf42bfbabdda
andreas.hausberger <andreas.hausberger@hpc.at>
2020-05-09 17:51:49 +0200

    [US #31] Added Load Event Service Method stub.

--- frontend/src/app/components/event-location/event-location.component.ts
+++ frontend/src/app/components/event-location/event-location.component.ts
@@ -13,7 +13,8 @@ export class EventLocationComponent implements OnInit {
   @Output() locationSaved = new EventEmitter<Location>();
 
   constructor(private eventService: EventService,
-              private sanitizer: DomSanitizer) { }
+              private sanitizer: DomSanitizer) {
+  }
 
   private searchComplete = false
   private addressSelected = false;
@@ -31,7 +32,7 @@ export class EventLocationComponent implements OnInit {
     this.selectionComplete = false;
     let element: any = document.getElementById('addressSearch');
     let value = element.value;
-    this.eventService.searchLocationInAPI(value).subscribe( results => {
+    this.eventService.searchLocationInAPI(value).subscribe(results => {
       this.searchResults = results;
       this.searchComplete = true;
       console.log(results);
@@ -42,12 +43,12 @@ export class EventLocationComponent implements OnInit {
 
   addressClicked(result: any) {
     let specificName = result.address.address29 ? result.address29 : result.address.road
-    let locationAddress = result.address.road + " " +  result.address.postcode + " " +  result.address.city;
-     let location: Location = new Location(null, specificName, locationAddress, result.address.postcode, result.lat, result.lon);
-     this.selectedLocation = location;
-     console.log(location);
-     this.createPreviewURL();
-     this.addressSelected = true;
+    let locationAddress = result.address.road + " " + result.address.postcode + " " + result.address.city;
+    let location: Location = new Location(null, specificName, locationAddress, result.address.postcode, result.lat, result.lon);
+    this.selectedLocation = location;
+    console.log(location);
+    this.createPreviewURL();
+    this.addressSelected = true;
   }
 
   saveButtonClicked() {
@@ -57,14 +58,14 @@ export class EventLocationComponent implements OnInit {
   }
 
   private createPreviewURL() {
-  let upperBBoxLat: number = this.selectedLocation.latitude - (-0.0002);
-  let lowerBBoxLat: number = this.selectedLocation.latitude - 0.0002;
-  let upperBBoxLon: number = this.selectedLocation.longitude - (-0.0002);
-  let lowerBBoxLon: number = this.selectedLocation.longitude - 0.0002;
-  let previewString: string = "https://www.openstreetmap.org/export/embed.html?bbox=" + lowerBBoxLon + "%2C" + lowerBBoxLat + "%2C" + upperBBoxLon + "%2C" + upperBBoxLat + "&amp;layer=mapnik&amp;marker=" + this.selectedLocation.longitude + "%2C" + this.selectedLocation.latitude;
+    let upperBBoxLat: number = this.selectedLocation.latitude - (-0.0002);
+    let lowerBBoxLat: number = this.selectedLocation.latitude - 0.0002;
+    let upperBBoxLon: number = this.selectedLocation.longitude - (-0.0002);
+    let lowerBBoxLon: number = this.selectedLocation.longitude - 0.0002;
+    let previewString: string = "https://www.openstreetmap.org/export/embed.html?bbox=" + lowerBBoxLon + "%2C" + lowerBBoxLat + "%2C" + upperBBoxLon + "%2C" + upperBBoxLat + "&amp;layer=mapnik&amp;marker=" + this.selectedLocation.longitude + "%2C" + this.selectedLocation.latitude;
 
-  console.log(previewString);
-  this.previewURL = this.sanitizer.bypassSecurityTrustResourceUrl(previewString)
-}
+    console.log(previewString);
+    this.previewURL = this.sanitizer.bypassSecurityTrustResourceUrl(previewString)
+  }
 
 }
