1c08d2c933b8893e3c468aadef5bf659d0a61b51
Yizhou "Andi" Cui <howdy@andicui.xyz>
2020-05-10 12:04:52 +0200

    [US #30] Fixed calendar offset on sundays.

--- frontend/src/app/components/weekly-calendar/weekly-calendar.component.ts
+++ frontend/src/app/components/weekly-calendar/weekly-calendar.component.ts
@@ -82,7 +82,10 @@ export class WeeklyCalendarComponent implements OnInit {
     let today = this.getToday();
 
     let beginOfWeek = new Date(today);
-    beginOfWeek.setDate(beginOfWeek.getDate() - (today.getDay() - 1));
+    // % is remainder and not mod in js. Therefore -1%7=-1. This is a workaround.
+    const weekOffset = (today.getDay() - 1 +7)%7;
+    console.log(weekOffset)
+    beginOfWeek.setDate(beginOfWeek.getDate() - weekOffset);
 
     let date = new Date(beginOfWeek);
     date.setDate(date.getDate() + offset * 7);
