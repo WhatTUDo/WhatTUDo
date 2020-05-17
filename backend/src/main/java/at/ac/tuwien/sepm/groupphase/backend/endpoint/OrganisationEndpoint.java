5a76f3f2cf9743739e875c41f17e3335e7ffa955
Luca Eichler <mail@luca-eichler.at>
2020-05-17 13:57:16 +0200

    #44 Fixed bug where the calendar ids would not be mapped to the DTO

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/OrganisationEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/OrganisationEndpoint.java
@@ -2,9 +2,12 @@ package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.OrganisationDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.CalendarMapper;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.OrganisationMapper;
+import at.ac.tuwien.sepm.groupphase.backend.entity.Calendar;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Organisation;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepm.groupphase.backend.service.CalendarService;
 import at.ac.tuwien.sepm.groupphase.backend.service.OrganisationService;
 import at.ac.tuwien.sepm.groupphase.backend.util.ValidationException;
 import io.swagger.annotations.ApiOperation;
@@ -17,8 +20,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Slf4j
@@ -29,6 +34,7 @@ public class OrganisationEndpoint {
     static final String BASE_URL = "/organisations";
     private final OrganisationService organisationService;
     private final OrganisationMapper organisationMapper;
+    private final CalendarService calendarService;
 
 
     @PutMapping
@@ -89,6 +95,34 @@ public class OrganisationEndpoint {
             return organisationMapper.organisationToOrganisationDto(organisationService.findById(id));
         } catch (NotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+        }
+    }
+
+    @PutMapping(value = "/{id}/calendars")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Add Calendars to an Organisation by ID", authorizations = {@Authorization(value = "apiKey")})
+    public OrganisationDto addCalToOrga(@PathVariable(value = "id") Integer orgaId, @RequestParam(value = "id") List<Integer> calendarIds) {
+        log.info("PUT " + BASE_URL + "/{}/calendars", orgaId);
+        try {
+            Collection<Calendar> calendars = calendarIds.stream().map(calendarService::findById).collect(Collectors.toList());
+            Organisation organisation = organisationService.addCalendars(organisationService.findById(orgaId), calendars);
+            return organisationMapper.organisationToOrganisationDto(organisation);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+        }
+    }
+
+    @DeleteMapping(value = "/{id}/calendars")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Remove Calendars from an Organisation by ID", authorizations = {@Authorization(value = "apiKey")})
+    public OrganisationDto removeCalFromOrga(@PathVariable(value = "id") Integer orgaId, @RequestParam(value = "id") List<Integer> calendarIds) {
+        log.info("DELETE " + BASE_URL + "/{}/calendars", orgaId);
+        try {
+            Collection<Calendar> calendars = calendarIds.stream().map(calendarService::findById).collect(Collectors.toList());
+            Organisation organisation = organisationService.removeCalendars(organisationService.findById(orgaId), calendars);
+            return organisationMapper.organisationToOrganisationDto(organisation);
         } catch (ServiceException e) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
         }
