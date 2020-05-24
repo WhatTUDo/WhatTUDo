6a08740c6e105cb221329e46fa0b1ef8ff56c0f5
andreas.hausberger <andreas.hausberger@hpc.at>
2020-05-24 13:24:07 +0200

    [US #57] Implemented Changes to User DTOs, some small fixes.

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/UserEndpoint.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
-import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.UserDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.IncomingUserDto;
+import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.LoggedInUserDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.UserMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
@@ -12,10 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
@@ -31,7 +29,7 @@ public class UserEndpoint {
     @CrossOrigin
     @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Create New User!")
-    public UserDto createNewUser(@RequestBody UserDto user) {
+    public LoggedInUserDto createNewUser(@RequestBody IncomingUserDto user) {
         log.info("POST " + BASE_URL, user);
         try {
             ApplicationUser newUser = userService.saveNewUser(userMapper.userDtoToApplicationUser(user));
@@ -50,7 +48,7 @@ public class UserEndpoint {
     @CrossOrigin
     @ResponseStatus(HttpStatus.OK)
     @ApiOperation("update user")
-    public UserDto updateUser(@RequestBody UserDto userDto){
+    public LoggedInUserDto updateUser(@RequestBody LoggedInUserDto userDto){
 //        final ApplicationUser user = (ApplicationUser) SecurityContextHolder.getContext()
 //            .getAuthentication()
 //            .getPrincipal();
@@ -58,7 +56,7 @@ public class UserEndpoint {
 //            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "A problem occurred while auth");
 //        }
         try {
-            return userMapper.applicationUserToUserDto(userService.updateUser(userMapper.userDtoToApplicationUser(userDto)));
+            return userMapper.applicationUserToUserDto(userService.updateUser(userMapper.loggedInUserDtoToApplicationUser(userDto)));
         } catch (ServiceException e){
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, e.getMessage());
         } catch (ValidationException e){
@@ -67,4 +65,6 @@ public class UserEndpoint {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
+
+
 }
--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/IncomingUserDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/IncomingUserDto.java
@@ -1,14 +1,14 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
-
+@NoArgsConstructor
+public class IncomingUserDto {
     private Integer id;
     private String name;
     private String email;
