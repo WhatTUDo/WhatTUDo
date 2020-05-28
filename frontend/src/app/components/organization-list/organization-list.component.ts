e92712ac27ce823c893a592f7b88552227007caf
andreas.hausberger <andreas.hausberger@hpc.at>
2020-05-28 16:59:30 +0200

    [US #89] Added List, Buttons for Organizations.

--- frontend/src/app/components/organization-list/organization-list.component.ts
+++ frontend/src/app/components/organization-list/organization-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OrganizationService} from "../../services/organization.service";
+import {Organization} from "../../dtos/organization";
 
 @Component({
   selector: 'app-organization-list',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationListComponent implements OnInit {
 
-  constructor() { }
+  organizations: Array<Organization>
+
+  constructor(
+    private organizationService: OrganizationService) { }
 
   ngOnInit(): void {
+
+    this.getOrganizations();
+  }
+
+  private getOrganizations() {
+    this.organizationService.getAll().subscribe( organizations => {
+      this.organizations = organizations;
+    },
+      error => {
+      console.warn(error);
+      })
+  }
+
+  private onClickedEdit(id: number) {
+    window.location.replace("/form/organization/" + id);
+  }
+
+  private onClickedDelete(id: number) {
+    alert("Delete not yet implemented. (clicked ID " + id + ")");
   }
 
 }
