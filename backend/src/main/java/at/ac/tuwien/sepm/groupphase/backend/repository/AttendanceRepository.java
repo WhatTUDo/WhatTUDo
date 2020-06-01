293856eb11510d19a7192af4c8f2d3aca9fd067d
Andreas Schuch <e11809629@student.tuwien.ac.at>
2020-06-01 10:56:34 +0200

    #87 added repository

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/AttendanceRepository.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.sepm.groupphase.backend.repository;
+
+import at.ac.tuwien.sepm.groupphase.backend.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Organization, Integer> {
+
+
+}
