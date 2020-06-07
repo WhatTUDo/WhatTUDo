1eeda2109431229d47b9cc4003957e86ab3bd555
Andreas Schuch <e11809629@student.tuwien.ac.at>
2020-06-07 14:03:06 +0200

    #104 started Service-Layer testing

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/servicetests/UserServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/servicetests/UserServiceTest.java
@@ -1,8 +1,11 @@
 package at.ac.tuwien.sepm.groupphase.backend.servicetests;
 
 
-import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepm.groupphase.backend.entity.*;
+import at.ac.tuwien.sepm.groupphase.backend.repository.AttendanceRepository;
+import at.ac.tuwien.sepm.groupphase.backend.repository.CalendarRepository;
 import at.ac.tuwien.sepm.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepm.groupphase.backend.service.AttendanceService;
 import at.ac.tuwien.sepm.groupphase.backend.service.impl.CustomUserDetailService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +14,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 
 @ExtendWith(SpringExtension.class)
@@ -21,9 +30,15 @@ public class UserServiceTest {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    CalendarRepository calendarRepository;
+    @Autowired
+    AttendanceRepository attendanceRepository;
 
     @Autowired
     CustomUserDetailService userService;
+    @Autowired
+    AttendanceService attendanceService;
 
     @Test
     public void when_savedUser_findAllUsers_shouldReturnListContainingUser() {
@@ -39,4 +54,32 @@ public class UserServiceTest {
         assert (user.getId() != null && user.getId() != 0);
 
     }
+
+    @Test
+    public void getRecommendedEvents_shouldReturn_correctEvent() {
+        ApplicationUser user = userService.saveNewUser(new ApplicationUser("TestUser 1", "testy1@test.com", "hunter2"));
+        Calendar calendar = calendarRepository.save(new Calendar("Test Calendar Service 3", Collections.singletonList(new Organization())));
+        List<Label> labels1 = new ArrayList<>();
+        List<Label> labels2 = new ArrayList<>();
+        List<Label> labels3 = new ArrayList<>();
+        Label label1 = new Label("TestLabel1");
+        Label label2 = new Label("TestLabel2");
+        labels1.add(label1);
+        labels2.add(label1);
+        labels2.add(label2);
+        labels3.add(label1);
+        Event event1 = new Event("Test Event 1", LocalDateTime.of(2021,1,1,15,30),LocalDateTime.of(2020,1,1,16,0),calendar);
+        event1.setLabels(labels1);
+        Event event2 = new Event("Test  Event 2", LocalDateTime.of(2021,1,2,15,30),LocalDateTime.of(2020,1,1,16,0),calendar);
+        event2.setLabels(labels2);
+        AttendanceStatus attend1 = attendanceRepository.save(new AttendanceStatus(user,event1,AttendanceStatusPossibilities.INTERESTED));
+        AttendanceStatus attend2 = attendanceRepository.save(new AttendanceStatus(user,event2,AttendanceStatusPossibilities.INTERESTED));
+        Event event3 = new Event("Test Event 3", LocalDateTime.of(2021,1,2,15,30),LocalDateTime.of(2020,1,1,16,0),calendar);
+        event3.setLabels(labels3);
+        Event recommendedEvent = userService.getRecommendedEvent(user.getId());
+        assert(recommendedEvent!=null);
+        assertEquals(recommendedEvent.getId(),event3.getId());
+
+    }
+
 }
