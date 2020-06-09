a1335ad8611ff37ebc49ed61b2f8fdda8860c2bc
Yizhou "Andi" Cui <howdy@andicui.xyz>
2020-06-09 15:10:11 +0200

    [Fix] Now Organization Form works well with invalid ID.

--- frontend/src/app/interceptors/error-interceptor.ts
+++ frontend/src/app/interceptors/error-interceptor.ts
@@ -7,7 +7,7 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import {Injectable} from "@angular/core"
-import {Observable, of} from "rxjs";
+import {Observable, of, throwError} from "rxjs";
 import {tap, catchError} from "rxjs/operators";
 import {FeedbackService} from "../services/feedback.service";
 
@@ -22,36 +22,36 @@ export class ErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
 
     return next.handle(req).pipe(
-      tap(evt => {
-        if (evt instanceof HttpResponse) {
-          if (evt.body && evt.body.success) {}
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          if (event.body && event.body.success) {}
         }
       }),
-      catchError((err: any) => {
-        if (err instanceof HttpErrorResponse) {
+      catchError((error: any) => {
+        if (error instanceof HttpErrorResponse) {
           try {
-           switch (err.status) {
+           switch (error.status) {
              case 400:
              case 405:
-               this.feedbackService.displayError(`Oops. This should not have happened. (${err.status})`, 'We are not sending a correct request to the server. Contact us, we are glad to fix this.');
+               this.feedbackService.displayError(`Oops. This should not have happened. (${error.status})`, 'We are not sending a correct request to the server. Contact us, we are glad to fix this.');
                break
              case 401:
-               this.feedbackService.displayError(`Login Required. (${err.status})`, 'You can only do this after you logged in.');
+               this.feedbackService.displayError(`Login Required. (${error.status})`, 'You can only do this after you logged in.');
                break
              case 403:
-               this.feedbackService.displayError(`Permission Denied. (${err.status})`, 'You are not allow to do that.');
+               this.feedbackService.displayError(`Permission Denied. (${error.status})`, 'You are not allow to do that.');
                break
              case 404:
-               this.feedbackService.displayError(`This does not exist. (${err.status})`, 'We cannot find what you are looking for in our database.');
+               this.feedbackService.displayError(`This does not exist. (${error.status})`, 'We cannot find what you are looking for in our database.');
                break
              default:
-               this.feedbackService.displayServerError(err);
+               this.feedbackService.displayServerError(error);
            }
           } catch (e) {
             this.feedbackService.displayError('Error!', 'An unexpected error occurred.');
           }
         }
-        return of(err);
+        return of(error);
       }));
 
   }
