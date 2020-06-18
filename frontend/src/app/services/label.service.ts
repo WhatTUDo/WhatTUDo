a08a8b05c6a4633b92021a490c2b71ed77433cfb
Yizhou "Andi" Cui <howdy@andicui.xyz>
2020-06-18 03:46:04 +0200

    Refactoring css button.

--- frontend/src/app/services/label.service.ts
+++ frontend/src/app/services/label.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {AuthRequest} from "../dtos/auth-request";
 import {Observable} from "rxjs";
 import {Label} from "../dtos/label";
 import {HttpClient} from "@angular/common/http";
@@ -22,6 +21,17 @@ export class LabelService {
 
   deleteLabel(id: number): Observable<void> {
     return this.httpClient.delete<void>(this.labelBaseUri + `/${id}`);
+  }
+
+  createLabel(name: string): Observable<Label> {
+    console.log('Post Label to Server', name);
+    let reducedElement = {
+      'id': null,
+      'name': name,
+      'events': null
+    };
+
+    return this.httpClient.post<Label>(this.labelBaseUri, reducedElement);
   }
 
 }
