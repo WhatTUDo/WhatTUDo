432006be59892c9020b0b6c3544943f9618597d2
Luca Eichler <eic.luca@gmail.com>
2020-06-19 16:47:24 +0200

    #174 Fixes bug

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/CalendarDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/CalendarDto.java
@@ -8,24 +8,26 @@ import lombok.*;
 import java.util.List;
 
 @Data
-@RequiredArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class CalendarDto extends PermissionDto {
-    @NonNull
     private Integer id;
-    @NonNull
     private String name;
-    @NonNull
     private List<Integer> organizationIds;
-    @NonNull
     private List<Integer> eventIds;
-    @NonNull
     private String description;
     private boolean canCreateEvents;
 
     @JsonProperty("coverImageUrl")
     private String calculateCoverImageUrl() {
         return CalendarEndpoint.BASE_URL + "/" + this.id + "/cover";
+    }
+
+    public CalendarDto(Integer id, String name, List<Integer> organizationIds, List<Integer> eventIds, String description) {
+        this.id = id;
+        this.name = name;
+        this.organizationIds = organizationIds;
+        this.eventIds = eventIds;
+        this.description = description;
     }
 
     public CalendarDto(Integer id, String name, List<Integer> organizationIds, List<Integer> eventIds) {
