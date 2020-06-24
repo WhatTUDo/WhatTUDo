be487ad599742d33cb6025f79969e346546a337d
Luca Eichler <eic.luca@gmail.com>
2020-06-24 21:14:36 +0200

    Fixed remove user from orga

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/ApplicationUser.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/ApplicationUser.java
@@ -32,7 +32,7 @@ public class ApplicationUser extends BaseEntity implements CustomUserDetails {
 
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.ALL}, fetch = FetchType.EAGER, orphanRemoval = true)
     // Eager necessary because of getAuthorities()
     private Set<OrganizationMembership> memberships = new HashSet<>();
 
